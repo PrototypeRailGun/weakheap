2f3d3cf7b020e172049102631fd5e17e34866c6d
PrototypeRailGun <52821033+PrototypeRailGun@users.noreply.github.com>
2022-02-27 17:17:16 +0300

    Performance tests have been conducted

--- benches/my_benchmark.rs
+++ benches/my_benchmark.rs
@@ -1,77 +1,186 @@
-/// Temporary performance tests.
-/// Full and detailed tests will be done later.
-use criterion::{black_box, criterion_group, criterion_main, Criterion};
+mod data;
+
+use criterion::{criterion_group, criterion_main, BenchmarkId, Criterion};
+use data::{long_comp_strings, WORDS};
+use std::collections::BinaryHeap;
 use weakheap::WeakHeap;
-//use std::collections::BinaryHeap;
 
-fn basics(elements: Vec<String>) {
-    let mut heap1: WeakHeap<String> = WeakHeap::from(elements.clone());
-    let mut heap2: WeakHeap<String> = WeakHeap::from(elements.clone());
+fn get_words(count: usize) -> Vec<String> {
+    WORDS[0..count].iter().map(|&w| w.to_string()).collect()
+}
+
+fn weakheap_sort(size: usize) -> Vec<String> {
+    let heap = WeakHeap::from(get_words(size));
+    heap.into_sorted_vec()
+}
+
+fn weakheap_push_pop(size: usize) -> Vec<String> {
+    let mut heap = WeakHeap::with_capacity(size * 2);
+    let data = get_words(size);
+    for w in data {
+        heap.push(w);
+        let x = heap.pop().unwrap();
+        heap.push(x);
+    }
+    let mut sorted = Vec::with_capacity(size);
+    while let Some(w) = heap.pop() {
+        sorted.push(w);
+    }
+    sorted
+}
+
+fn weakheap_append(size: usize) -> Vec<String> {
+    let mut heap1 = WeakHeap::from(get_words(size / 2));
+    let mut heap2 = WeakHeap::from(get_words(size / 2));
     heap1.append(&mut heap2);
+    heap1.into_vec()
 }
 
-fn criterion_benchmark(c: &mut Criterion) {
-    c.bench_function("fib 20", |b| {
-        b.iter(|| {
-            basics(black_box(vec![
-                String::from("truhdkiufyhnsryfsjnf"),
-                String::from("gdsuhdjjjidjsdjdsjud"),
-                String::from("5783uhffffffffffffffffff"),
-                String::from("fhjsjfj"),
-                String::from("gdsuhdjjj744748748"),
-                String::from("46yfhjnamzndgansjehan"),
-                String::from("truhdkiufyhnsryfsjnf"),
-                String::from("gdsuhdjjjidjsdjdsjud"),
-                String::from("5783uhffffffffffffffffff"),
-                String::from("fhjsjfj"),
-                String::from("gdsuhdjjj744748748"),
-                String::from("46yfhjnamzndgansjehan"),
-                String::from("truhdkiufyhnsryfsjnf"),
-                String::from("gdsuhdjjjidjsdjdsjud"),
-                String::from("5783uhffffffffffffffffff"),
-                String::from("fhjsjfj"),
-                String::from("gdsuhdjjj744748748"),
-                String::from("46yfhjnamzndgansjehan"),
-                String::from("truhdkiufyhnsryfsjnf"),
-                String::from("gdsuhdjjjidjsdjdsjud"),
-                String::from("5783uhffffffffffffffffff"),
-                String::from("fhjsjfj"),
-                String::from("gdsuhdjjj744748748"),
-                String::from("46yfhjnamzndgansjehan"),
-                String::from("truhdkiufyhnsryfsjnf"),
-                String::from("gdsuhdjjjidjsdjdsjud"),
-                String::from("5783uhffffffffffffffffff"),
-                String::from("fhjsjfj"),
-                String::from("gdsuhdjjj744748748"),
-                String::from("46yfhjnamzndgansjehan"),
-                String::from("truhdkiufyhnsryfsjnf"),
-                String::from("gdsuhdjjjidjsdjdsjud"),
-                String::from("5783uhffffffffffffffffff"),
-                String::from("fhjsjfj"),
-                String::from("gdsuhdjjj744748748"),
-                String::from("46yfhjnamzndgansjehan"),
-                String::from("truhdkiufyhnsryfsjnf"),
-                String::from("gdsuhdjjjidjsdjdsjud"),
-                String::from("5783uhffffffffffffffffff"),
-                String::from("fhjsjfj"),
-                String::from("gdsuhdjjj744748748"),
-                String::from("46yfhjnamzndgansjehan"),
-                String::from("truhdkiufyhnsryfsjnf"),
-                String::from("gdsuhdjjjidjsdjdsjud"),
-                String::from("5783uhffffffffffffffffff"),
-                String::from("fhjsjfj"),
-                String::from("gdsuhdjjj744748748"),
-                String::from("46yfhjnamzndgansjehan"),
-                String::from("truhdkiufyhnsryfsjnf"),
-                String::from("gdsuhdjjjidjsdjdsjud"),
-                String::from("5783uhffffffffffffffffff"),
-                String::from("fhjsjfj"),
-                String::from("gdsuhdjjj744748748"),
-                String::from("46yfhjnamzndgansjehan"),
-            ]))
-        })
+fn weakheap_longcomp(_size: usize) -> Vec<String> {
+    let heap = WeakHeap::from(long_comp_strings());
+    heap.into_sorted_vec()
+}
+
+fn binheap_sort(size: usize) -> Vec<String> {
+    let heap = BinaryHeap::from(get_words(size));
+    heap.into_sorted_vec()
+}
+
+fn binheap_push_pop(size: usize) -> Vec<String> {
+    let mut heap = BinaryHeap::with_capacity(size * 2);
+    let data = get_words(size);
+    for w in data {
+        heap.push(w);
+        let x = heap.pop().unwrap();
+        heap.push(x);
+    }
+    let mut sorted = Vec::with_capacity(size);
+    while let Some(w) = heap.pop() {
+        sorted.push(w);
+    }
+    sorted
+}
+
+fn binheap_append(size: usize) -> Vec<String> {
+    let mut heap1 = BinaryHeap::from(get_words(size / 2));
+    let mut heap2 = BinaryHeap::from(get_words(size / 2));
+    heap1.append(&mut heap2);
+    heap1.into_vec()
+}
+
+fn binheap_longcomp(_size: usize) -> Vec<String> {
+    let heap = BinaryHeap::from(long_comp_strings());
+    heap.into_sorted_vec()
+}
+
+fn quicksort(size: usize) -> Vec<String> {
+    let mut vec = get_words(size);
+    vec.sort_unstable();
+    vec
+}
+
+fn bench_sorting(c: &mut Criterion) {
+    let mut group = c.benchmark_group("Sorting");
+
+    for i in 1..=9 {
+        let size = i * 100;
+        group.bench_with_input(BenchmarkId::new("Binary Heap", size), &size, |b, s| {
+            b.iter(|| binheap_sort(*s))
+        });
+        group.bench_with_input(BenchmarkId::new("Weak Heap", size), &size, |b, s| {
+            b.iter(|| weakheap_sort(*s))
+        });
+        group.bench_with_input(BenchmarkId::new("Quicksort", size), &size, |b, s| {
+            b.iter(|| quicksort(*s))
+        });
+    }
+
+    for i in 2..=8 {
+        let size = i * 500;
+        group.bench_with_input(BenchmarkId::new("Binary Heap", size), &size, |b, s| {
+            b.iter(|| binheap_sort(*s))
+        });
+        group.bench_with_input(BenchmarkId::new("Weak Heap", size), &size, |b, s| {
+            b.iter(|| weakheap_sort(*s))
+        });
+        group.bench_with_input(BenchmarkId::new("Quicksort", size), &size, |b, s| {
+            b.iter(|| quicksort(*s))
+        });
+    }
+
+    group.finish();
+}
+
+fn bench_basics(c: &mut Criterion) {
+    let mut group = c.benchmark_group("Push & Pop");
+
+    for i in 1..=9 {
+        let size = i * 100;
+        group.bench_with_input(BenchmarkId::new("Binary Heap", size), &size, |b, s| {
+            b.iter(|| binheap_push_pop(*s))
+        });
+        group.bench_with_input(BenchmarkId::new("Weak Heap", size), &size, |b, s| {
+            b.iter(|| weakheap_push_pop(*s))
+        });
+    }
+
+    for i in 2..=8 {
+        let size = i * 500;
+        group.bench_with_input(BenchmarkId::new("Binary Heap", size), &size, |b, s| {
+            b.iter(|| binheap_push_pop(*s))
+        });
+        group.bench_with_input(BenchmarkId::new("Weak Heap", size), &size, |b, s| {
+            b.iter(|| weakheap_push_pop(*s))
+        });
+    }
+
+    group.finish();
+}
+
+fn bench_append(c: &mut Criterion) {
+    let mut group = c.benchmark_group("Append");
+
+    for i in 1..=9 {
+        let size = i * 100;
+        group.bench_with_input(BenchmarkId::new("Binary Heap", size), &size, |b, s| {
+            b.iter(|| binheap_append(*s))
+        });
+        group.bench_with_input(BenchmarkId::new("Weak Heap", size), &size, |b, s| {
+            b.iter(|| weakheap_append(*s))
+        });
+    }
+
+    for i in 2..=8 {
+        let size = i * 500;
+        group.bench_with_input(BenchmarkId::new("Binary Heap", size), &size, |b, s| {
+            b.iter(|| binheap_append(*s))
+        });
+        group.bench_with_input(BenchmarkId::new("Weak Heap", size), &size, |b, s| {
+            b.iter(|| weakheap_append(*s))
+        });
+    }
+
+    group.finish();
+}
+
+fn bench_long_comp(c: &mut Criterion) {
+    let mut group = c.benchmark_group("Strings with long comparison");
+    let size = 54;
+    group.bench_with_input(BenchmarkId::new("Binary Heap", size), &size, |b, s| {
+        b.iter(|| binheap_longcomp(*s))
     });
+    group.bench_with_input(BenchmarkId::new("Weak Heap", size), &size, |b, s| {
+        b.iter(|| weakheap_longcomp(*s))
+    });
+
+    group.finish();
 }
 
-criterion_group!(benches, criterion_benchmark);
+criterion_group!(
+    benches,
+    bench_sorting,
+    bench_basics,
+    bench_append,
+    bench_long_comp
+);
 criterion_main!(benches);
