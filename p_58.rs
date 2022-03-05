daa7961a70a6dff4f198c3a3995c4e0543d28e80
Egor Starovoitov <52821033+PrototypeRailGun@users.noreply.github.com>
2022-03-05 13:22:58 +0300

    Delete p_58.rs

--- p_58.rs
+++
@@ -1,6 +0,0 @@
-impl Solution {
-    pub fn length_of_last_word(s: String) -> i32 {
-        // map_or is faster then unwrap
-        s.split_ascii_whitespace().last().map_or(0, |w| w.len()) as i32
-    }
-}
